45ff7188fc4234c0c7122ae7bf77111eb2b8fd45
Darius A. Faorughy <darius.faroughy@rutgers.edu>
2026-05-03 22:01:10 -0700

    promoted new Evals. old evaluation deprecated

--- scripts/multi-run-interactive-codex_mini.sh
+++ scripts/multi-run-interactive-codex_mini.sh
@@ -37,26 +37,29 @@ if [[ ! -f "${CONFIG}" ]]; then
 fi
 
 TASKS=(
-  sus-16-034_shape-TChiWZ
-  sus-16-034_sim-TChiWZ
-  sus-16-046_shape-T5Wg
-  sus-16-046_shape-TChiWg
-  sus-16-046_sim-T5Wg
-  sus-16-046_sim-TChiWg
-  sus-16-047_shape-T5Wg_highHT
-  sus-16-047_shape-T5Wg_lowHT
-  sus-16-047_shape-T6gg_highHT
-  sus-16-047_shape-T6gg_lowHT
-  sus-16-047_sim-T5Wg_highHT
-  sus-16-047_sim-T5Wg_lowHT
-  sus-16-047_sim-T6gg_highHT
-  sus-16-047_sim-T6gg_lowHT
-  sus-16-051_shape-T2tt_SRG
-  sus-16-051_shape-T2bW_SRG
-  sus-16-051_shape-T2tt_comp
-  sus-16-051_sim-T2tt_SRG
-  sus-16-051_sim-T2bW_SRG
-  sus-16-051_sim-T2tt_comp
+sus-16-034_shape-TChiWZ
+sus-16-034_sim-TChiWZ
+sus-16-046_shape-TChiWg
+sus-16-046_sim-T5Wg
+sus-16-046_sim-TChiWg
+sus-16-047_shape-T5Wg_highHT
+sus-16-047_shape-T5Wg_lowHT
+sus-16-047_shape-T6gg_highHT
+sus-16-047_shape-T6gg_lowHT
+sus-16-047_sim-T5Wg_highHT
+sus-16-047_sim-T5Wg_lowHT
+sus-16-047_sim-T6gg_highHT
+sus-16-047_sim-T6gg_lowHT
+sus-16-051_shape-T2tt_SRG
+sus-16-051_shape-T2tt_SRG
+sus-16-051_shape-T2bW_SRG
+sus-16-051_shape-T2bW_SRG
+sus-16-051_shape-T2tt_comp
+sus-16-051_sim-T2tt_SRG
+sus-16-051_sim-T2tt_SRG
+sus-16-051_sim-T2bW_SRG
+sus-16-051_sim-T2bW_SRG
+sus-16-051_sim-T2tt_comp
 )
 
 cd "${REPO_ROOT}"
